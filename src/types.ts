965b21f68fe1b1d73fd573ff1987e0b78afccfcd
David Boyne <boyneyy123@gmail.com>
2024-11-25 12:17:46 +0000

    feat(plugin): added feature to add channels into eventcatalog from asyncapi file

--- src/types.ts
+++ src/types.ts
@@ -3,7 +3,6 @@ export interface MessageOperations {
   write: (payload: any, options: any) => Promise<void>;
   version: (id: string) => Promise<any>;
   get: (id: string, version: string) => Promise<any>;
-  remove: (id: string, version: string) => Promise<void>;
   addSchema: (id: string, schema: any, version: any) => Promise<void>;
 }
 
